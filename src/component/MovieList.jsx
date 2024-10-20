68af57a94ca18e6d9d198e09a215077e7a7e84c7
Nguyen Hoang Hai <hainguyenhoang1205@gmail.com>
2024-10-20 14:39:03 +0700

    fix: hover on movie item

--- src/component/MovieList.jsx
+++ src/component/MovieList.jsx
@@ -64,37 +64,6 @@ const MovieList = ({ title, data, scrollToTop }) => {
   );
 };
 
-// return (
-//   <div className="text-white p-10 mb-10">
-//     <h2 className="uppercase text-xl mb-4">{title}</h2>
-//     <Carousel
-//       responsive={responsive}
-//       className="flex items-center space-x-4"
-//     >
-//       {data.map((item) => (
-//         <div
-//           key={item.id}
-//           className="w-[200px] h-[300px] relative group flex-shrink-0"
-//         >
-//           <div className="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full cursor-pointer">
-//             <div className="absolute top-0 left-0 w-full h-full bg-black/40" />
-//             <img
-//               src={`${import.meta.env.VITE_IMG_URL}${item.poster_path}`}
-//               alt={item.title}
-//               className="w-full h-full object-cover"
-//             />
-//             <div className="absolute bottom-4 left-2">
-//               <p className="uppercase text-md">
-//                 {item.title || item.original_title}
-//               </p>
-//             </div>
-//           </div>
-//         </div>
-//       ))}
-//     </Carousel>
-//   </div>
-// );
-
 MovieList.propTypes = {
   title: PropTypes.string.isRequired,
   data: PropTypes.array.isRequired,
